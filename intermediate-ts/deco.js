79c99dcf1e07b70326bbeb53517db6d10174f38b
yongsoocho <yongsoocho578@gmail.com>
2022-12-12 18:00:09 +0900

    #15 deco factory

--- intermediate-ts/deco.js
+++ intermediate-ts/deco.js
@@ -1,3 +1,5 @@
+// f(g(x)) ----> f () { return g () } , g: 데코레이터 함수
+// g ----> f(g(x)) , f: 데코레이터 팩토리 (목적: 인자전달, param 전달)
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -6,35 +8,64 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 // 1. 데코레이터는 함수다
 function Controller(constructor) {
-    console.log("Controller : ", constructor);
+    // console.log("Controller : ", constructor);
+    // return (
+    //   constructor: Function,
+    //   propertyKey: string,
+    //   descriptor: PropertyDescriptor
+    // ) => {
+    //   console.log(constructor);
+    // };
 }
 function Get(params) {
-    console.log("[GET] ", params);
+    // console.log("[GET] ", params);
 }
 function Post(params) {
-    console.log("[GET] ", params);
+    console.log("[POST] deco deci Factory : ", params);
+    return function (target, propertyKey, descriptor) {
+        console.log("[POST] deco deco Func : ", target, propertyKey, descriptor);
+        target.getReq();
+        target[propertyKey]();
+    };
 }
 function Column(params) {
-    console.log("Column !!", params);
+    // console.log("Column !!", params);
+}
+function UseGuard() {
+    // console.log("UseGuard Factory : ");
+    // return (
+    //   constructor: Function,
+    //   propertyKey: string,
+    //   descriptor: PropertyDescriptor
+    // ) => {
+    //   console.log("UseGuard deco Func : ", constructor, propertyKey, descriptor);
+    // };
 }
 // 2. 데코레이터는 무조건 class 와 만 같이쓴다. (내부 외부, 멤버 변수, 메소드, 파라미터...)
 var ExampleController = /** @class */ (function () {
     function ExampleController(email) {
         this._email = email;
     }
-    ExampleController.prototype.getReq = function () { };
-    ExampleController.prototype.postReq = function () { };
+    ExampleController.prototype.getReq = function () {
+        console.log("getReq method process!");
+    };
+    // Factory { f(g(x)) 여기서 f 역할, g deco func } -> top to bottom
+    // deco func bottom to top
+    ExampleController.prototype.postReq = function () {
+        console.log("postReq method process!");
+    };
     __decorate([
         Column("email")
     ], ExampleController.prototype, "_email");
     __decorate([
-        Get("/api/v1/user")
+        Get("/user")
     ], ExampleController.prototype, "getReq");
     __decorate([
-        Post("/api/v1/board")
+        Post("/board"),
+        UseGuard()
     ], ExampleController.prototype, "postReq");
     ExampleController = __decorate([
-        Controller
+        Controller("/api/v1")
     ], ExampleController);
     return ExampleController;
 }());
--- intermediate-ts/deco.ts
+++ intermediate-ts/deco.ts
@@ -1,22 +1,50 @@
+// f(g(x)) ----> f () { return g () } , g: 데코레이터 함수
+// g ----> f(g(x)) , f: 데코레이터 팩토리 (목적: 인자전달, param 전달)
+
 // 1. 데코레이터는 함수다
-function Controller(constructor: Function) {
-  console.log("Controller : ", constructor);
+function Controller(constructor: any): any {
+  // console.log("Controller : ", constructor);
+  // return (
+  //   constructor: Function,
+  //   propertyKey: string,
+  //   descriptor: PropertyDescriptor
+  // ) => {
+  //   console.log(constructor);
+  // };
 }
 
 function Get(params: any): any {
-  console.log("[GET] ", params);
+  // console.log("[GET] ", params);
 }
 
 function Post(params: any): any {
-  console.log("[GET] ", params);
+  console.log("[POST] deco deci Factory : ", params);
+
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    console.log("[POST] deco deco Func : ", target, propertyKey, descriptor);
+
+    target.getReq();
+    target[propertyKey]();
+  };
 }
 
 function Column(params: any): any {
-  console.log("Column !!", params);
+  // console.log("Column !!", params);
+}
+
+function UseGuard(): any {
+  // console.log("UseGuard Factory : ");
+  // return (
+  //   constructor: Function,
+  //   propertyKey: string,
+  //   descriptor: PropertyDescriptor
+  // ) => {
+  //   console.log("UseGuard deco Func : ", constructor, propertyKey, descriptor);
+  // };
 }
 
 // 2. 데코레이터는 무조건 class 와 만 같이쓴다. (내부 외부, 멤버 변수, 메소드, 파라미터...)
-@Controller
+@Controller("/api/v1")
 class ExampleController {
   @Column("email")
   private _email: string;
@@ -25,11 +53,18 @@ class ExampleController {
     this._email = email;
   }
 
-  @Get("/api/v1/user")
-  getReq() {}
+  @Get("/user")
+  getReq() {
+    console.log("getReq method process!");
+  }
 
-  @Post("/api/v1/board")
-  postReq() {}
+  // Factory { f(g(x)) 여기서 f 역할, g deco func } -> top to bottom
+  // deco func bottom to top
+  @Post("/board")
+  @UseGuard()
+  postReq() {
+    console.log("postReq method process!");
+  }
 }
 
 // 3. 런타임에 클래스에 붙어서 실행되는 함수 = 데코레이터 -> @ -> new Class() 인스턴스화 없이 실행

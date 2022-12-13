056784918ff534897dc1fae8c92fcb2fac4e6a14
yongsoocho <yongsoocho578@gmail.com>
2022-12-13 09:42:26 +0900

    extends constructor

--- intermediate-ts/deco.js
+++ intermediate-ts/deco.js
@@ -1,5 +1,20 @@
 // f(g(x)) ----> f () { return g () } , g: 데코레이터 함수
 // g ----> f(g(x)) , f: 데코레이터 팩토리 (목적: 인자전달, param 전달)
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,14 +23,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 // 1. 데코레이터는 함수다
 function Controller(constructor) {
-    // console.log("Controller : ", constructor);
-    // return (
-    //   constructor: Function,
-    //   propertyKey: string,
-    //   descriptor: PropertyDescriptor
-    // ) => {
-    //   console.log(constructor);
-    // };
+    return /** @class */ (function (_super) {
+        __extends(class_1, _super);
+        function class_1() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this._email = "http://www...";
+            _this._name = "yongsoo";
+            return _this;
+        }
+        return class_1;
+    }(constructor));
 }
 function Get(params) {
     // console.log("[GET] ", params);
@@ -44,13 +61,16 @@ function UseGuard() {
 // 2. 데코레이터는 무조건 class 와 만 같이쓴다. (내부 외부, 멤버 변수, 메소드, 파라미터...)
 var ExampleController = /** @class */ (function () {
     function ExampleController(email) {
-        this._email = email;
+        if (email)
+            this._email = email;
     }
     ExampleController.prototype.getReq = function () {
         console.log("getReq method process!");
     };
     // Factory { f(g(x)) 여기서 f 역할, g deco func } -> top to bottom
     // deco func bottom to top
+    // @Post("/board")
+    // @UseGuard()
     ExampleController.prototype.postReq = function () {
         console.log("postReq method process!");
     };
@@ -60,15 +80,13 @@ var ExampleController = /** @class */ (function () {
     __decorate([
         Get("/user")
     ], ExampleController.prototype, "getReq");
-    __decorate([
-        Post("/board"),
-        UseGuard()
-    ], ExampleController.prototype, "postReq");
     ExampleController = __decorate([
-        Controller("/api/v1")
+        Controller
     ], ExampleController);
     return ExampleController;
 }());
 // 3. 런타임에 클래스에 붙어서 실행되는 함수 = 데코레이터 -> @ -> new Class() 인스턴스화 없이 실행
 // new ExampleClass();
 // 4. 유추 -> 뭔가 데코레이터에서 함수 안에 데이터를 조작할 수 있을거 같다..
+var test = new ExampleController("email");
+console.log(test);
--- intermediate-ts/deco.ts
+++ intermediate-ts/deco.ts
@@ -2,15 +2,11 @@
 // g ----> f(g(x)) , f: 데코레이터 팩토리 (목적: 인자전달, param 전달)
 
 // 1. 데코레이터는 함수다
-function Controller(constructor: any): any {
-  // console.log("Controller : ", constructor);
-  // return (
-  //   constructor: Function,
-  //   propertyKey: string,
-  //   descriptor: PropertyDescriptor
-  // ) => {
-  //   console.log(constructor);
-  // };
+function Controller(constructor: { new (email: string): any }): any {
+  return class extends constructor {
+    _email = "http://www...";
+    _name = "yongsoo";
+  };
 }
 
 function Get(params: any): any {
@@ -44,13 +40,13 @@ function UseGuard(): any {
 }
 
 // 2. 데코레이터는 무조건 class 와 만 같이쓴다. (내부 외부, 멤버 변수, 메소드, 파라미터...)
-@Controller("/api/v1")
+@Controller
 class ExampleController {
   @Column("email")
   private _email: string;
 
   constructor(email: string) {
-    this._email = email;
+    if (email) this._email = email;
   }
 
   @Get("/user")
@@ -60,8 +56,8 @@ class ExampleController {
 
   // Factory { f(g(x)) 여기서 f 역할, g deco func } -> top to bottom
   // deco func bottom to top
-  @Post("/board")
-  @UseGuard()
+  // @Post("/board")
+  // @UseGuard()
   postReq() {
     console.log("postReq method process!");
   }
@@ -71,3 +67,6 @@ class ExampleController {
 // new ExampleClass();
 
 // 4. 유추 -> 뭔가 데코레이터에서 함수 안에 데이터를 조작할 수 있을거 같다..
+
+const test = new ExampleController("email");
+console.log(test);

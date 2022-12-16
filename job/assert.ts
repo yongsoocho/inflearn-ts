a7cdd7ac1123c9fbe2700bcf88c44d6d6ee4e324
yongsoocho <yongsoocho578@gmail.com>
2022-12-16 23:15:23 +0900

    #19 type assertion

--- job/assert.ts
+++ job/assert.ts
@@ -1,2 +1,15 @@
-const testText = "hello world";
-console.log(testText);
+// const testText = document.querySelector("#someText")!;
+
+// const testText = document.querySelector("#someText") as HTMLElement;
+// console.log(testText?.id);
+
+interface IExam {
+  value: number;
+}
+const example: object = { value: 1 };
+// console.log(example.value);
+
+// 1 as
+console.log((example as IExam).value);
+// 2 <>
+// console.log(<IExam>example);

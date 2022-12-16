a1088df0787c270ab05e4b2e44d65d66e1384819
yongsoocho <yongsoocho578@gmail.com>
2022-12-17 00:27:49 +0900

    #23 import export

---
+++ job/main.ts
@@ -0,0 +1,3 @@
+import * as MATH from "./module1";
+
+console.log(MATH.add(1, 2), MATH.minus2(3, 1));
---
+++ job/module1.ts
@@ -0,0 +1,19 @@
+export function add(a: number, b: number) {
+  return a + b;
+}
+
+export function minus(a: number, b: number) {
+  return a - b;
+}
+
+export function minus1(a: number, b: number) {
+  return a - b;
+}
+
+export function minus2(a: number, b: number) {
+  return a - b;
+}
+
+export function minus3(a: number, b: number) {
+  return a - b;
+}

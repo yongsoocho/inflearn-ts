d6f09009c072bf6cbc544a70160ead478fd4e62b
yongsoocho <yongsoocho578@gmail.com>
2022-12-23 01:54:56 +0900

    adapter

---
+++ practice/adapter.ts
@@ -0,0 +1,30 @@
+interface ICalculator {
+import CalculatorImpl from './1/calculator.impl';
+  // add(a: number, b: number): number;
+  addAandB(a: number, b: number): number;
+
+  // minus(a: number, b: number): number;
+  minusAandB(a: number, b: number): number;
+}
+
+class CalculatorImpl implements ICalculator {
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  minus(a: number, b: number): number {
+    return a - b;
+  }
+}
+// 상속
+class CalculatorImplAdapter extends CalculatorImpl implements ICalculator {
+  addAandB(a: number, b: number): number {
+    return this.add(a, b)
+  };
+
+  minusAandB(a: number, b: number): number {
+    return this.minus(a, b)
+  }
+}
+
+const cal: ICalculator = new CalculatorImpl();
